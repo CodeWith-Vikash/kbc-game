ad3555d59ae173787685659e9c4f1b81cee06591
vikash <vp3637637@gmail.com>
2024-03-17 20:11:28 +0530

    first commit

---
+++ src/components/Data.jsx
@@ -0,0 +1,142 @@
+const questions = [
+  {
+    "questionText": "What is the capital of Japan?",
+    "answerOptions": [
+      {"answerText": "Beijing", "isCorrect": false},
+      {"answerText": "Tokyo", "isCorrect": true},
+      {"answerText": "Seoul", "isCorrect": false},
+      {"answerText": "Bangkok", "isCorrect": false}
+    ]
+  },
+  {
+    "questionText": "Who painted the Mona Lisa?",
+    "answerOptions": [
+      {"answerText": "Michelangelo", "isCorrect": false},
+      {"answerText": "Pablo Picasso", "isCorrect": false},
+      {"answerText": "Leonardo da Vinci", "isCorrect": true},
+      {"answerText": "Vincent van Gogh", "isCorrect": false}
+    ]
+  },
+  {
+    "questionText": "Which planet is known as the Red Planet?",
+    "answerOptions": [
+      {"answerText": "Venus", "isCorrect": false},
+      {"answerText": "Mars", "isCorrect": true},
+      {"answerText": "Mercury", "isCorrect": false},
+      {"answerText": "Jupiter", "isCorrect": false}
+    ]
+  },
+  {
+    "questionText": "Who wrote 'Romeo and Juliet'?",
+    "answerOptions": [
+      {"answerText": "Jane Austen", "isCorrect": false},
+      {"answerText": "Charles Dickens", "isCorrect": false},
+      {"answerText": "Mark Twain", "isCorrect": false},
+      {"answerText": "William Shakespeare", "isCorrect": true}
+    ]
+  },
+  {
+    "questionText": "What is the chemical symbol for water?",
+    "answerOptions": [
+      {"answerText": "CO2", "isCorrect": false},
+      {"answerText": "O2", "isCorrect": false},
+      {"answerText": "H2O", "isCorrect": true},
+      {"answerText": "N2", "isCorrect": false},
+    ]
+  },
+  {
+    "questionText": "Which continent is the largest by land area?",
+    "answerOptions": [
+      {"answerText": "North America", "isCorrect": false},
+      {"answerText": "Asia", "isCorrect": true},
+      {"answerText": "South America", "isCorrect": false},
+      {"answerText": "Africa", "isCorrect": false},
+    ]
+  },
+  {
+    "questionText": "Who discovered penicillin?",
+    "answerOptions": [
+      {"answerText": "Albert Einstein", "isCorrect": false},
+      {"answerText": "Marie Curie", "isCorrect": false},
+      {"answerText": "Isaac Newton", "isCorrect": false},
+      {"answerText": "Alexander Fleming", "isCorrect": true},
+    ]
+  },
+  {
+    "questionText": "What is the currency of Germany?",
+    "answerOptions": [
+      {"answerText": "Euro", "isCorrect": true},
+      {"answerText": "Pound Sterling", "isCorrect": false},
+      {"answerText": "Dollar", "isCorrect": false},
+      {"answerText": "Yen", "isCorrect": false},
+    ]
+  },
+  {
+    "questionText": "Who was the first man to step on the moon?",
+    "answerOptions": [
+      {"answerText": "Buzz Aldrin", "isCorrect": false},
+      {"answerText": "Neil Armstrong", "isCorrect": true},
+      {"answerText": "Yuri Gagarin", "isCorrect": false},
+      {"answerText": "John Glenn", "isCorrect": false},
+    ]
+  },
+  {
+    "questionText": "What is the largest mammal in the world?",
+    "answerOptions": [
+      {"answerText": "African Elephant", "isCorrect": false},
+      {"answerText": "Giraffe", "isCorrect": false},
+      {"answerText": "Hippopotamus", "isCorrect": false},
+      {"answerText": "Blue Whale", "isCorrect": true}
+    ]
+  },
+  {
+    "questionText": "Who invented the telephone?",
+    "answerOptions": [
+      {"answerText": "Thomas Edison", "isCorrect": false},
+      {"answerText": "Nikola Tesla", "isCorrect": false},
+      {"answerText": "Alexander Graham Bell", "isCorrect": true},
+      {"answerText": "Guglielmo Marconi", "isCorrect": false},
+    ]
+  },
+  {
+    "questionText": "Which country is known as the Land of the Rising Sun?",
+    "answerOptions": [
+      {"answerText": "Japan", "isCorrect": true},
+      {"answerText": "South Korea", "isCorrect": false},
+      {"answerText": "China", "isCorrect":false},
+      {"answerText": "India", "isCorrect": false},
+    ]
+  },
+  {
+    "questionText": "Who wrote 'To Kill a Mockingbird'?",
+    "answerOptions": [
+      {"answerText": "John Steinbeck", "isCorrect": false},
+      {"answerText": "Harper Lee", "isCorrect": true},
+      {"answerText": "F. Scott Fitzgerald", "isCorrect": false},
+      {"answerText": "Ernest Hemingway", "isCorrect": false},
+    ]
+  },
+  {
+    "questionText": "What is the tallest mountain in the world?",
+    "answerOptions": [
+      {"answerText": "K2", "isCorrect": false},
+      {"answerText": "Kangchenjunga", "isCorrect": false},
+      {"answerText": "Mount Everest", "isCorrect": true},
+      {"answerText": "Makalu", "isCorrect": false}
+    ]
+  },
+  {
+    "questionText": "Who is known as the 'Father of Modern Physics'?",
+    "answerOptions": [
+      {"answerText": "Isaac Newton", "isCorrect": false},
+      {"answerText": "Niels Bohr", "isCorrect": false},
+      {"answerText": "Galileo Galilei", "isCorrect": false},
+      {"answerText": "Albert Einstein", "isCorrect": true}
+    ]
+  }
+]
+
+
+
+
+export default questions
